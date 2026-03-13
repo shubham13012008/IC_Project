f43f1b43f10d7ce2b037a99f3f8882e06957bed9
Lokesh <lokeshkag67@gmail.com>
2026-03-13 23:27:53 +0530

    Admin Page Added

---
+++ admin.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+ int name_comparison(char str1[], char str2[]){
+        int i = 0;
+        while(str1[i] != '\0' && str1[i] == str2[i]){
+            i++;
+        }
+        return str1[i] - str2[i];
+    }
+int main() {
+    char username[20],password[20];
+    printf("\n Admin Login ");
+    printf("\n Enter Admin ID- ");
+    scanf("%s",username);
+    printf(" Enter Password- ");
+    scanf("%s",password);
+   
+    if(name_comparison(username,"admin") == 0 && name_comparison(password, "iitj123")==0){
+        printf("\nLogin Successful");
+        return 1;
+    }
+
+    else{
+        printf("\n Invalid Credentials ");
+    }
+
+
+return 0;
+}
---
+++ train.h
@@ -0,0 +1,7 @@
+#ifndef TRAIN_H
+#define TRAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#endif
