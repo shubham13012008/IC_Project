fa01c2b97ba290ba9e3ea21af6178945ac17c615
niranjansingh6 <niranjansingh6375086014@gmail.com>
2026-04-19 23:24:02 +0530

    added connecting train algo

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ We have used multiple concepts that we have studied in the class to make this pr
 It has multiple features like searching for trains, booking tickets, cancelling tickets, adding trains, deleting trains, editing train details, viewing stats, etc.
 We have used alogrithm to search for the trains in case if a direct train is not available then the code searches for the connecting train that can get the passenger to their destination and we also sort the connecting trains if there are more than one then we show the trains according to the ascending order of prices.
 
+Run command for the project:-
+gcc main.c admin.c user.c booking.c file_io.c -o train_system
+./train_system.exe
 
 It has 2 Portals :-
 1. User Portal
@@ -26,10 +29,13 @@ Admin Portal has 4 features :-
 
 
 
-
+Team Members:-
 Parth Suryawanshi(B25ME1056)
 Lokesh(B25MT1022)
 Chetan Deora(B25EE1011)
 Deepak Kumar(B25EE1011)
 Niranjan Singh(B25CI1033)
 Shubham Prajapat(B25CH1039)
+
+
+
--- user.c
+++ user.c
@@ -34,15 +34,86 @@ void grahakMenu() {
         }
       }
 
+// Connecting Trains Algo
       if (matchCount == 0) {
-          printf("No trains found for this route and date.\n");
-      } else {
-          int choice;
-          printf("\nEnter 1 to proceed to booking, or 0 to go back: ");
-          scanf("%d", &choice);
-          if (choice == 1) {
-             bookTicket();
+        printf("No direct trains found. Searching for connecting trains...\n");
+
+        typedef struct {
+          int t1;
+          int t2;
+          float totalFare;
+        } Route;
+        Route connect[100];
+        int cf = 0; // connectionFound
+        // finding a new train from the start  to the middle on samee date
+        for (int i = 0; i < trainCount; i++) {
+          if (strcmp(trains[i].kahanSe, s) == 0 &&
+              strcmp(trains[i].tarik, dt) == 0) {
+            for (int j = 0; j < trainCount; j++) {
+              // finding a new train from the middle to the destinations
+              if (i != j &&
+                  strcmp(trains[j].kahanSe, trains[i].kahanTak) == 0 &&
+                  strcmp(trains[j].kahanTak, d) == 0 &&
+                  strcmp(trains[j].tarik, dt) == 0) {
+                // ensuring that there is time to catch the train
+                if (trains[j].depTime >= trains[i].arrTime && cf < 100) {
+                  connect[cf].t1 = i;
+                  connect[cf].t2 = j;
+                  connect[cf].totalFare =
+                      trains[i].priceSleeper + trains[j].priceSleeper;
+                  cf++;
+                }
+              }
+             }
           }
+          }
+
+        if (cf == 0) {
+          printf("No connecting trains found for this route and date.\n");
+        } else {
+          // Sorting the  connecting routes based on  minimum price
+          for (int m = 0; m < cf - 1; m++) {
+            for (int n = 0; n < cf - m - 1; n++) {
+              if (connect[n].totalFare > connect[n + 1].totalFare) {
+                Route temp = connect[n];
+                connect[n] = connect[n + 1];
+                connect[n + 1] = temp;
+              }
+            }
+          }
+
+          for (int k = 0; k < cf; k++) {
+            int i = connect[k].t1;
+            int j = connect[k].t2;
+            printf("\nConnection Option %d\n", k + 1);
+            printf(
+                "  Station 1 : Train No %d (%s)  //%s to %s  //Dep: %04d, Arr: "
+                "%04d  //AC: %.2f, SL: %.2f\n",
+                trains[i].trainNo, trains[i].naam, trains[i].kahanSe,
+                trains[i].kahanTak, trains[i].depTime, trains[i].arrTime,
+                trains[i].priceAC, trains[i].priceSleeper);
+            printf(
+                "  Station 2 : Train No %d (%s)  //%s to %s  //Dep: %04d, Arr: "
+                "%04d  //AC: %.2f, SL: %.2f\n",
+                trains[j].trainNo, trains[j].naam, trains[j].kahanSe,
+                trains[j].kahanTak, trains[j].depTime, trains[j].arrTime,
+                trains[j].priceAC, trains[j].priceSleeper);
+          }
+
+          int niranjan;
+          printf("\nEnter 1 to proceed to booking, or 0 to go back: ");
+          scanf("%d", &niranjan);
+          if (niranjan == 1) {
+            bookTicket();
+          }
+        }
+      } else {
+        int niranjan;
+        printf("\nEnter 1 to proceed to booking, or 0 to go back: ");
+        scanf("%d", &niranjan);
+        if (niranjan == 1) {
+          bookTicket();
+        }
       }
       break;
     }
